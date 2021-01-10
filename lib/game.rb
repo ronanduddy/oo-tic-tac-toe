fbc650d5f113303899f90625a74b66349940e0f0
ronanduddy <ronanduddy@live.ie>
2021-01-10 16:19:38 +0000

    Create main game loop

--- lib/game.rb
+++ lib/game.rb
@@ -7,6 +7,32 @@ class Game
   end
 
   def run
-    Grid.new(@stdout).render
+    game_over = false
+    grid = Grid.new(@stdout)
+    coordinates = ''
+
+    loop do
+      grid.render
+      grid.prompt
+      coordinates = @stdin.gets
+
+      if valid?(coordinates)
+        game_over = true
+      else
+        @stdout.puts("#{coordinates} is invalid")
+      end
+
+      break if game_over
+    end
+
+    @stdout.puts("#{coordinates} - good move bye!")
+  end
+
+  private
+
+  def valid?(coordinates)
+    alpha, digit = coordinates.split('')
+
+    alpha.match?(/[A-C]/) && digit.match?(/[1-3]/)
   end
 end
--- spec/lib/game_spec.rb
+++ spec/lib/game_spec.rb
@@ -1,39 +1,72 @@
 require 'game'
 
 RSpec.describe Game do
+  let(:game) { described_class.new(mock_stdin, mock_stdout) }
   let(:mock_stdin) { StringIO.new }
   let(:mock_stdout) { StringIO.new }
 
-  let(:grid) do
-    <<~STR
-        1  2  3
-        __ __ __
-    A  |  |  |  |
-       |__|__|__|
-    B  |  |  |  |
-       |__|__|__|
-    C  |  |  |  |
-       |__|__|__|
-
-    Enter your move >
-    STR
-  end
-
-  context '#run' do
-    subject(:run) { described_class.new(mock_stdin, mock_stdout).run }
-
+  describe '#run' do
     context 'when the user enters good coordinates' do
+      let(:grid) do
+        <<~STR
+            1  2  3
+            __ __ __
+        A  |  |  |  |
+           |__|__|__|
+        B  |  |  |  |
+           |__|__|__|
+        C  |  |  |  |
+           |__|__|__|
+
+        Enter your move >
+        A1 - good move bye!
+        STR
+      end
+
+      before { allow(mock_stdin).to receive(:gets).and_return('A1') }
+
       it 'renders the grid, prompts user and exits' do
-        run
+        expect(mock_stdin).to receive(:gets)
+        game.run
         expect(mock_stdout.string).to eq grid
       end
     end
 
-    # When the user enters bad coordinates,the application returns a
-    # message about not understanding the message,then waits for good input.
+    context 'when the user enters bad coordinates' do
+      before { allow(mock_stdin).to receive(:gets).and_return('Z1', 'A1') }
 
-    # The application doesn’t quit until the user enters good information.
-    # Coordinates are a row letter combined with a column number,
-    # for example: A2, B3.
+      let(:grid) do
+        <<~STR
+            1  2  3
+            __ __ __
+        A  |  |  |  |
+           |__|__|__|
+        B  |  |  |  |
+           |__|__|__|
+        C  |  |  |  |
+           |__|__|__|
+
+        Enter your move >
+        Z1 is invalid
+            1  2  3
+            __ __ __
+        A  |  |  |  |
+           |__|__|__|
+        B  |  |  |  |
+           |__|__|__|
+        C  |  |  |  |
+           |__|__|__|
+
+        Enter your move >
+        A1 - good move bye!
+        STR
+      end
+
+      it 'renders the grid, prompts and quits upon a correct move' do
+        expect(mock_stdin).to receive(:gets)
+        game.run
+        expect(mock_stdout.string).to eq grid
+      end
+    end
   end
 end
