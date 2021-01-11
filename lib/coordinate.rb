9c022ca41eb6879ffd4d928abe6e0617fd9bafdb
ronanduddy <ronanduddy@live.ie>
2021-01-11 13:18:19 +0000

    Remove `attr_reader` for Coordinate
    Seems pointless

--- lib/coordinate.rb
+++ lib/coordinate.rb
@@ -1,15 +1,13 @@
 class Coordinate
-  attr_reader :row, :column
-
   def initialize(input)
     @row, @column = input.split('')
   end
 
   def to_s
-    row + column
+    @row + @column
   end
 
   def valid?
-    row.match?(/[A-C]/) && column.match?(/[1-3]/)
+    @row.match?(/[A-C]/) && @column.match?(/[1-3]/)
   end
 end
