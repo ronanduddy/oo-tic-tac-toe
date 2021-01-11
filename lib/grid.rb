11f7adf50040c193458ca33798cdde2b46e6af5b
ronanduddy <ronanduddy@live.ie>
2021-01-11 12:46:28 +0000

    Render proper grid after valid move

--- lib/grid.rb
+++ lib/grid.rb
@@ -1,22 +1,33 @@
 class Grid
-  def initialize(messager)
-    @messager = messager
-  end
-
-  def render
-    grid =
-            <<~STR
+  BOARD = <<~STR
               1  2  3
               __ __ __
-          A  |  |  |  |
+          A  |%{A1} |%{A2} |%{A3} |
              |__|__|__|
-          B  |  |  |  |
+          B  |%{B1} |%{B2} |%{B3} |
              |__|__|__|
-          C  |  |  |  |
+          C  |%{C1} |%{C2} |%{C3} |
              |__|__|__|
 
             STR
 
-    @messager.tell(grid)
+  def initialize(messager)
+    @messager = messager
+    @coordinates = {}
+  end
+
+  def render
+    @messager.tell(board_state)
+  end
+
+  def add(coordinate)
+    @coordinates[coordinate.to_s.intern] = 'X'
+  end
+
+  private
+
+  def board_state
+    @coordinates.default = ' '
+    sprintf(BOARD, @coordinates)
   end
 end
--- spec/lib/grid_spec.rb
+++ spec/lib/grid_spec.rb
@@ -1,12 +1,13 @@
 require 'grid'
 require 'shared_context/messager'
+require 'shared_context/coordinate'
 
 RSpec.describe Grid do
   include_context 'messager'
 
   let(:grid) { described_class.new(messager) }
 
-  context '#render' do
+  describe '#render' do
     let(:grid_output) do
       <<~STR
         1  2  3
@@ -22,6 +23,32 @@ RSpec.describe Grid do
     end
 
     it 'renders an empty grid' do
+      grid.render
+      expect(mock_stdout.string).to eq grid_output
+    end
+  end
+
+  describe '#add' do
+    include_context 'coordinate'
+
+    let(:value) { 'B2' }
+
+    let(:grid_output) do
+      <<~STR
+        1  2  3
+        __ __ __
+    A  |  |  |  |
+       |__|__|__|
+    B  |  |X |  |
+       |__|__|__|
+    C  |  |  |  |
+       |__|__|__|
+
+      STR
+    end
+
+    it 'adds coordinate to coordinates' do
+      grid.add(coordinate)
       grid.render
       expect(mock_stdout.string).to eq grid_output
     end
